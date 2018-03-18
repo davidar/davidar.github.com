1f2982c839621fd716196af994a2b0197b5e4fd2
David A Roberts <d@vidr.cc>
2018-03-18 20:17:10 +1000

    rm external scripts

--- _includes/quotes.js
+++ _includes/quotes.js
@@ -1,15 +1,21 @@
-$(window).load(function() {
-    $('#quote').hide();
-    $('#quote').html('<p id="quote-body" class="lead"></p><p style="text-align:right">&mdash; <a id="quote-src" href="#" rel="nofollow"></a></p>');
-    var updateQuote = function() {
-        var i = Math.floor(Math.random() * quotes.length);
-        $('#quote-body').html(       quotes[i][0]);
-        $('#quote-src').html(        quotes[i][1]);
-        $('#quote-src').attr('href', quotes[i][2]);
-        $('#quote').fadeIn('slow');
-    };
+function updateQuote() {
+  var i = Math.floor(Math.random() * quotes.length);
+  document.getElementById('quote-body').innerHTML = quotes[i][0];
+  document.getElementById('quote-src').innerHTML  = quotes[i][1];
+  document.getElementById('quote-src').href       = quotes[i][2];
+  document.getElementById('quote').style.opacity = 1;
+}
+
+function main() {
+  window.setTimeout(updateQuote, 1000);
+  window.setInterval(function() {
+    document.getElementById('quote').style.opacity = 0;
     window.setTimeout(updateQuote, 1000);
-    window.setInterval(function() {
-        $('#quote').fadeOut('slow', updateQuote);
-    }, 15000);
-});
+  }, 15000);
+}
+
+if(document.readyState === 'complete') {
+  main();
+} else {
+  window.addEventListener('load', main);
+}
