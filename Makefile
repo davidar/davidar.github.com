5347d79e2b5c6d235b38b08b1cdb1d56a11de1a5
David A Roberts <d@vidr.cc>
2021-08-04 11:14:00 +0000

    fix fonts loading after quote fade in

--- Makefile
+++
@@ -1,8 +0,0 @@
-all:
-	yui-compressor _includes/quotes.js > _includes/quotes.min.js
-
-build: all
-	bundle exec jekyll build
-
-serve: all
-	bundle exec jekyll serve
--- _includes/quotes.js
+++ _includes/quotes.js
@@ -1,17 +1,17 @@
 function updateQuote() {
-  var i = Math.floor(Math.random() * quotes.length);
-  document.getElementById('quote-body').innerHTML = quotes[i][0];
-  document.getElementById('quote-src').innerHTML  = quotes[i][1];
-  document.getElementById('quote-src').href       = quotes[i][2];
-  document.getElementById('quote').style.opacity = 1;
+  document.getElementById('quote').style.opacity = 0;
+  window.setTimeout(function() {
+    var i = Math.floor(Math.random() * quotes.length);
+    document.getElementById('quote-body').innerHTML = quotes[i][0];
+    document.getElementById('quote-src').innerHTML  = quotes[i][1];
+    document.getElementById('quote-src').href       = quotes[i][2];
+    document.getElementById('quote').style.opacity = 1;
+  }, 1000);
 }
 
 function main() {
-  window.setTimeout(updateQuote, 1000);
-  window.setInterval(function() {
-    document.getElementById('quote').style.opacity = 0;
-    window.setTimeout(updateQuote, 1000);
-  }, 15000);
+  window.setTimeout(updateQuote, 1500);
+  window.setInterval(updateQuote, 15000);
 }
 
 if(document.readyState === 'complete') {
